1ebeacdb9a543c4522f702e5ffc5610fdc1cc450
Jagatheeskumar 03014 TAGTES <jagatheeskumar@tagtes.com>
2023-01-05 18:56:34 +0530

    Change mail id details

--- nodeangularversioncheck.sh
+++ nodeangularversioncheck.sh
@@ -4,7 +4,7 @@ npmversion=$(npm --v)
 ngversion=$(ng version | grep ^"Angular CLI" | cut -d ":" -f 2 | sed 's/ //')
 nodeversion=$(node --version)
 
-cat <<EOF | mail -s "Date: $(date) Report on $(hostname)" sysadmin@tagtes.com
+cat <<EOF | mail -s "Date: $(date) Report on $(hostname -f)" <mail-id>
 NPM Version: $npmversion
 Angular CLI Version: $ngversion
 echo "Node Version: $nodeversion
